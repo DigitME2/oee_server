1b3cd491b4be6edbd4a69103fcc5ba712f3007eb
Sam Bannister <sambannister@gmail.com>
2019-03-18 16:10:44 +0000

    Plotly graph reacts to dropdown changes on end job screen

--- app/oee_displaying/graph_helper.py
+++ app/oee_displaying/graph_helper.py
@@ -1,13 +1,16 @@
 from plotly.offline import plot
+from plotly.graph_objs import Layout
 from datetime import datetime
-from app.default.models import Activity, Machine, UPTIME_CODE
+from app.default.models import Activity, Machine, UPTIME_CODE, UNEXPLAINED_DOWNTIME_CODE, ERROR_1_CODE
 
-import pandas as pd
+import plotly
 import plotly.figure_factory as ff
 
 
 def create_machine_gantt(machine, graph_start, graph_end):
     """ Create a gantt chart of the usage of a single machine, between the two timestamps provided"""
+
+    # todo dont rely on hardcoded codes eg "unexplained"
 
     # Get the machine's activities between the two times
     activities = Activity.query \
@@ -16,6 +19,8 @@ def create_machine_gantt(machine, graph_start, graph_end):
         .filter(Activity.timestamp_start <= graph_end).all()
     if len(activities) == 0:
         return "No machine activity between these times"
+    if machine is None:
+        return "This machine does not exist"
 
     # Add each activity to a dictionary, to add to the graph
     df = []
@@ -32,16 +37,16 @@ def create_machine_gantt(machine, graph_start, graph_end):
         df.append(dict(Task=activity.code.short_description,
                        Start=datetime.fromtimestamp(start),
                        Finish=datetime.fromtimestamp(end),
-                       Code=activity.code.short_description,
+                       Code=activity.activity_code,
                        Activity_id=activity.id,
                        hoverinfo="test"))
 
     graph_title = "{machine_name} OEE".format(machine_name=machine.name)
-    colours = {'unexplained': 'rgb(128, 128, 128)',
-               'uptime': 'rgb(0, 255, 128)',
-               'error1': 'rgb(255,64,0)',
-               'error2': 'rgb(255,0,0)',
-               'error3': 'rgb(255,255,0)'}
+    colours = {UNEXPLAINED_DOWNTIME_CODE: 'rgb(128, 128, 128)',
+               UPTIME_CODE: 'rgb(0, 255, 128)',
+               ERROR_1_CODE: 'rgb(255,64,0)',
+               4: 'rgb(255,0,0)',
+               5: 'rgb(255,255,0)'}
     fig = ff.create_gantt(df,
                           title=graph_title,
                           group_tasks=True,
@@ -57,13 +62,14 @@ def create_machine_gantt(machine, graph_start, graph_end):
 
     # Hide the range selector
     fig['layout']['xaxis']['rangeselector']['visible'] = False
-    fig['layout']['annotations'] = [dict(x='2009-02-01', y=0, text="This is a label", showarrow=False, font=dict(color='white'))]
     return plot(fig, output_type="div", include_plotlyjs=True)
 
 
 def create_all_machines_gantt(graph_start, graph_end):
     """ Creates a gantt plot of OEE for all machines in the database between given times"""
     machines = Machine.query.all()
+    if len(machines) == 0:
+        return "No machines found"
     df = []
     for machine in machines:
         activities = Activity.query \
@@ -104,3 +110,90 @@ def create_all_machines_gantt(graph_start, graph_end):
     # Hide the range selector
     fig['layout']['xaxis']['rangeselector']['visible'] = False
     return plot(fig, output_type="div", include_plotlyjs=True)
+
+
+def create_shift_end_gantt(machine, activities):
+    """ Create a gantt chart of the usage of a single machine, between the two timestamps provided"""
+
+    # todo dont rely on hardcoded codes eg "unexplained"
+
+    if len(activities) == 0:
+        return "No machine activity between these times"
+    if machine is None:
+        return "This machine does not exist"
+
+
+    # Add each activity to a dictionary, to add to the graph
+    # Do this in two separate loops so that the entries requiring explanation are first in the dictionary,
+    # putting them on the upper level in the graph
+    df = []
+    annotations = []
+    for act in activities:
+        if act.explanation_required:
+            task = "Explanation<br>Required"
+            start = act.timestamp_start
+            end = act.timestamp_end
+            df.append(dict(Task=task,
+                           Start=datetime.fromtimestamp(start),
+                           Finish=datetime.fromtimestamp(end),
+                           Code=act.explanation_required,
+                           Activity_id=act.id,
+                           hoverinfo="test"))
+            # Javascript relies on the first character being the ud_index, so alter the script accordingly
+            text = "{start}<br>Explanation<br>Required".format(
+                start=datetime.fromtimestamp(act.timestamp_start).strftime('%H:%M'))
+            position = datetime.fromtimestamp((start + end) / 2)
+            annotations.append(dict(x=position, y=1.7, text=text, showarrow=False, font=dict(color='black')))
+    for act in activities:
+        if not act.explanation_required:
+            task = "Uptime"
+            start = act.timestamp_start
+            end = act.timestamp_end
+            df.append(dict(Task=task,
+                           Start=datetime.fromtimestamp(start),
+                           Finish=datetime.fromtimestamp(end),
+                           Code=act.explanation_required,
+                           Activity_id=act.id,
+                           hoverinfo="test"))
+
+
+
+    colours = {True: 'rgb(255, 0, 0)',
+               False: 'rgb(0, 255, 128)'}
+    fig = ff.create_gantt(df,
+                          title="",
+                          group_tasks=True,
+                          colors=colours,
+                          index_col='Code',
+                          bar_width=0.4,
+                          show_colorbar=False,
+                          width=1800)
+
+    # Create a layout object using the layout automatically created
+    layout = Layout(fig['layout'])
+
+    layout.annotations = annotations
+    layout.showlegend = False
+    layout.yaxis.showticklabels = False
+    layout.xaxis.rangeselector.visible = False
+    layout.xaxis.showline = True
+
+    layout.autosize = False
+    # layout.width = 1200
+    # layout.height = 300
+    layout.margin = dict(l=0, r=0, b=50, t=0, pad=0)
+
+    # Pass the changed layout back to fig
+    fig['layout'] = layout
+
+    config = {'responsive': True}
+
+
+
+
+    #fig['layout'].update(responsive=True, autosize=False, width=1200, height=300, margin=dict(l=110, r=50, b=50, t=50, pad=4))
+    return plot(fig, output_type="div", include_plotlyjs=True, config=config)
+
+
+def sort_activities(act):
+    return act.explanation_required
