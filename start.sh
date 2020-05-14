78c6a972a46ab907222e72d12c75e01debdcc5c0
Sam Bannister <sambannister@gmail.com>
2020-05-14 16:48:18 +0100

    Removed passwords

--- start.sh
+++ start.sh
@@ -5,11 +5,6 @@ export FLASK_APP=oee_webapp.py
 # URI address of the database. If this is given, all other database variables are ignored
 export DATABASE_URI=sqlite:///test.db
 
-#export DATABASE_USER=postgres
-#export DATABASE_ADDRESS=localhost
-#export DATABASE_PORT=5432
-#export DATABASE_NAME=prod
-#export DATABASE_PASSWORD=
 
 source venv/bin/activate
 #flask run
