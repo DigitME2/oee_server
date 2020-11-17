db5dd8cc18f79c4f3936ccd2ae5cf711ad99873d
sambannister@gmail.com <Gudlol69#>
2020-11-17 00:18:02 +0000

    get redis address from config.py

--- app/extensions.py
+++ app/extensions.py
@@ -3,8 +3,10 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from config import Config
+
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = 'login.login'
-celery_app = Celery('tasks', broker='redis://localhost:6379', backend='redis://localhost:6379')
+celery_app = Celery('tasks', broker=Config.REDIS_ADDRESS, backend=Config.REDIS_ADDRESS)
