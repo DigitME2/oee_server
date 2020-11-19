7fe29b5b015b51edbca56c8aa9af279e6ec51914
sambannister@gmail.com <Gudlol69#>
2020-11-19 15:45:31 +0000

    changed logging level

--- app/default/scheduler.py
+++ app/default/scheduler.py
@@ -3,27 +3,27 @@ from flask import current_app
 
 from app.default.db_helpers import create_scheduled_activities
 from app.extensions import celery_app
-from app.oee_monitoring.machine_simulator import simulate_machines
+from app.default.machine_simulator import simulate_machines
 from config import Config
 
 
 @celery_app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
-    current_app.logger.debug("Setting up periodic tests")
+    current_app.logger.info("Setting up periodic tests")
     sender.add_periodic_task(crontab(hour=3, minute=0),
                              daily_machine_schedule_task.s())
     if Config.DEMO_MODE:
-        sender.add_periodic_task(10, simulate_machine_action_task.s())
+        sender.add_periodic_task(Config.DATA_SIMULATION_FREQUENCY, simulate_machine_action_task.s())
 
 
 @celery_app.task()
 def daily_machine_schedule_task():
-    current_app.logger.info("Running machine schedule celery task")
+    current_app.logger.debug("Running machine schedule celery task")
     create_scheduled_activities()
     return True
 
 
 @celery_app.task()
 def simulate_machine_action_task():
-    current_app.logger.info("Running machine simulation celery task")
+    current_app.logger.debug("Running machine simulation celery task")
     simulate_machines()
