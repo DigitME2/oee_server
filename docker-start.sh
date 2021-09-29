3a1144c684a2476cbf43157c6ccc76780eb86baf
Sam Bannister <sambannister@gmail.com>
2021-09-29 16:47:22 +0100

    increased gunicorn worker timeout

--- docker-start.sh
+++ docker-start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Something was causing the worker to hang and timeout. setting timeout to 1000 fixed this. Don't know why.
 
-exec gunicorn -b :8000 -m 007 "app:create_app()"
+exec gunicorn -b :8000 -m 007 --timeout 1000 "app:create_app()"
 
