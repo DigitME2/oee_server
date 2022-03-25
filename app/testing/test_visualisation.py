3dfecd4e3fd256c0d101cda3192e0e5683c68fd3
Sam Bannister <sambannister@gmail.com>
2022-03-25 11:35:40 +0000

    added tests

--- app/testing/test_visualisation.py
+++ app/testing/test_visualisation.py
@@ -2,7 +2,8 @@ import unittest
 from datetime import datetime, time, timedelta
 
 from app.testing.base import BaseTest
-from app.visualisation.graphs import create_machine_gantt
+from app.visualisation.graphs import create_machine_gantt, create_multiple_machines_gantt, create_schedules_gantt, \
+    create_oee_line
 
 
 class VisualisationTest(BaseTest):
@@ -11,8 +12,14 @@ class VisualisationTest(BaseTest):
         with self.app.app_context():
             day_start = datetime.combine(date=datetime.now().date(), time=time(hour=0, minute=0, second=0, microsecond=0))
             day_end = day_start + timedelta(days=1)
-            graph = create_machine_gantt(machine_id=1, graph_start=day_start, graph_end=day_end)
-            self.assertIsNotNone(graph)
+            gantt_graph = create_machine_gantt(machine_id=1, graph_start=day_start, graph_end=day_end)
+            self.assertIsNotNone(gantt_graph)
+            oee_line_graph = create_oee_line(day_start.date(), day_end.date(), [1])
+            self.assertIsNotNone(oee_line_graph)
+            multiple_gantt_graph = create_multiple_machines_gantt(graph_start=day_start, graph_end=day_end, machine_ids=[1])
+            self.assertIsNotNone(multiple_gantt_graph)
+            schedules_gantt = create_schedules_gantt(graph_start=day_start, graph_end=day_end, machine_ids=[1])
+            self.assertIsNotNone(schedules_gantt)
 
 
 if __name__ == '__main__':
