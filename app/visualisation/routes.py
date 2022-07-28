c6d6b2fa04d963d1eacd309870f895599a1e2bfb
Sam Bannister <sambannister@gmail.com>
2022-07-28 16:19:29 +0100

    Put the graph-creation code directly in the apq endpoint function

--- app/visualisation/routes.py
+++ app/visualisation/routes.py
@@ -1,12 +1,16 @@
 from datetime import datetime, timedelta
 
+import plotly.graph_objects as go
 from flask import abort, request, render_template, current_app
 from flask_login import login_required
 from flask_wtf import FlaskForm
+from plotly.graph_objs import Layout
+from plotly.offline import plot
 from sqlalchemy import inspect
-from wtforms import DateField, TimeField, SubmitField, SelectField
-from wtforms.validators import DataRequired, NoneOf
+from wtforms import DateField, SubmitField, SelectField
+from wtforms.validators import DataRequired
 
+from app.data_analysis.oee.oee import get_daily_machine_oee
 from app.default.models import Machine, MachineGroup, Settings
 from app.extensions import db
 from app.visualisation import bp
@@ -43,9 +47,33 @@ def apq():
 
     # If the form has been submitted we need to create the graph
     if form.validate_on_submit():
-        graph = create_oee_line(graph_start_date=form.start_date.data,
-                                graph_end_date=form.end_date.data,
-                                machine_ids=[form.key.data])
+        machine_ids = [form.key.data]
+        graph_start_date = form.start_date.data,
+        graph_end_date = form.end_date.data
+
+        d = Settings.query.get(1).first_start.date()
+        dates = [d + timedelta(days=x) for x in range((graph_end_date - d).days + 1)]
+        if len(dates) == 0:
+            return 0
+        fig = go.Figure()
+        machine_ids.sort()
+        for machine_id in machine_ids:
+            machine = Machine.query.get(machine_id)
+            machine_oee_figures = []
+            for d in dates:
+                machine_oee_figures.append(get_daily_machine_oee(machine_id=machine_id, date=d))
+            fig.add_trace(go.Scatter(x=dates, y=machine_oee_figures, name=machine.name, mode='lines+markers'))
+
+        layout = Layout()
+        layout.xaxis.update(range=[graph_start_date, graph_end_date])
+        layout.xaxis.tickformat = '%a %d-%m-%Y'
+        layout.xaxis.showgrid = False
+        layout.xaxis.dtick = 86400000  # Space between ticks = 1 day
+        fig.layout = layout
+        graph = plot(fig,
+                     output_type="div",
+                     include_plotlyjs=True,
+                     config={"showLink": False})
     else:
         graph = None
 
