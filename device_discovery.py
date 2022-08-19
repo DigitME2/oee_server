6893b0be6b98d90eeed7088cd8a0296f7327be7d
Sam Bannister <sambannister@gmail.com>
2022-08-19 12:17:22 +0100

    modified device discovery to use port number

--- device_discovery.py
+++ device_discovery.py
@@ -1,13 +1,15 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from time import sleep
+from config import Config
 
 """ Listens for a device broadcast and responds, so the device can discover this server. """
 
-PORT = 8090
+BROADCAST_PORT = 8090  # This should match the port set in the app
 EXPECTED_TEXT = "DISCOVER_OEE_SERVER_REQUEST"
+EXPECTED_RESPONSE = "DISCOVER_OEE_SERVER_RESPONSE"
 
 s = socket(AF_INET, SOCK_DGRAM)
-s.bind(('', PORT))
+s.bind(('', BROADCAST_PORT))
 
 while 1:
     data, addr = s.recvfrom(1024)  # Wait for a packet
@@ -15,5 +17,6 @@ while 1:
 
     if data.decode("utf-8") == EXPECTED_TEXT:
         print("Responding...")
-        s.sendto("DISCOVER_OEE_SERVER_RESPONSE".encode("utf-8"), addr)
+        s.sendto((EXPECTED_RESPONSE + ":" + Config.EXTERNAL_PORT).encode("utf-8"), addr)
+
     sleep(5)
