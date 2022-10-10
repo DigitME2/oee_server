58816e39219bd68c82514be8ed328b133e1e941c
Sam Bannister <sambannister@gmail.com>
2022-10-10 16:09:06 +0100

    Check for sudo privilege before running

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with sudo"
+  exit
+fi
+
 # Install requirements
 echo "Installing apt packages..."
-apt update -qq && apt install npm redis virtualenv nginx -qq
+apt-get update -qqq
+apt-get install -qq -y npm redis virtualenv nginx > /dev/null
 
 # Copy default config
 cp ./example-confs/config.py.example config.py
@@ -20,28 +27,28 @@ virtualenv --quiet venv
 
 # Copy systemd files
 echo "Configuring systemd services..."
-sudo cp ./example-confs/oee_celery_beat.service /etc/systemd/system
-sudo cp ./example-confs/oee_server.service /etc/systemd/system
-sudo cp ./example-confs/oee_discovery.service /etc/systemd/system
-sudo cp ./example-confs/oee_celery.service /etc/systemd/system
+cp ./example-confs/oee_celery_beat.service /etc/systemd/system
+cp ./example-confs/oee_server.service /etc/systemd/system
+cp ./example-confs/oee_discovery.service /etc/systemd/system
+cp ./example-confs/oee_celery.service /etc/systemd/system
 
 # Edit default values (user, working directory) in example systemd files
-sudo sed -i 's&(USERNAME)&'$USER'&g' /etc/systemd/system/oee_server.service /etc/systemd/system/oee_celery.service /etc/systemd/system/oee_celery_beat.service /etc/systemd/system/oee_discovery.service
-sudo sed -i 's&/home/user/oee_server&'$PWD'&g' /etc/systemd/system/oee_server.service /etc/systemd/system/oee_celery.service /etc/systemd/system/oee_celery_beat.service /etc/systemd/system/oee_discovery.service
+sed -i 's&(USERNAME)&'$USER'&g' /etc/systemd/system/oee_server.service /etc/systemd/system/oee_celery.service /etc/systemd/system/oee_celery_beat.service /etc/systemd/system/oee_discovery.service
+sed -i 's&/home/user/oee_server&'$PWD'&g' /etc/systemd/system/oee_server.service /etc/systemd/system/oee_celery.service /etc/systemd/system/oee_celery_beat.service /etc/systemd/system/oee_discovery.service
 
 # Enable & start services
-sudo systemctl daemon-reload
-sudo systemctl enable oee_server oee_discovery oee_celery oee_celery_beat
-sudo systemctl start oee_server oee_discovery oee_celery oee_celery_beat
+systemctl daemon-reload
+systemctl enable oee_server oee_discovery oee_celery oee_celery_beat
+systemctl start oee_server oee_discovery oee_celery oee_celery_beat
 
 # Set up Nginx
 echo "Setting up Nginx..."
-sudo unlink /etc/nginx/sites-enabled/default
-sudo cp ./example-confs/nginx-conf-example /etc/nginx/sites-available
-sudo ln -s /etc/nginx/sites-available/nginx-conf-example /etc/nginx/sites-enabled
-sudo nginx -t
-sudo nginx -s reload
-sudo ufw allow 'Nginx Full'
+unlink /etc/nginx/sites-enabled/default
+cp ./example-confs/nginx-conf-example /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/nginx-conf-example /etc/nginx/sites-enabled
+nginx -t
+nginx -s reload
+ufw allow 'Nginx Full'
 
 read -p "Complete. Press any key to continue"
 
