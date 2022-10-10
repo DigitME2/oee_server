ee50e1210a051f93031aca812bd81f9481c0e36c
Sam Bannister <sambannister@gmail.com>
2022-10-10 12:35:22 +0100

    Altered log messages shown during installation for user friendliness

--- install.sh
+++ install.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 # Install requirements
-apt update && apt install -y npm redis virtualenv nginx
+echo "Installing apt packages..."
+apt update -qq && apt install npm redis virtualenv nginx -qq
 
 # Copy default config
 cp ./example-confs/config.py.example config.py
 
 # install npm package in /app/static
+echo "Running npm install..."
 npm --prefix ./app/static install ./app/static
 
 # Set up virtual environment
+echo "Creating python virtual environment..."
 virtualenv venv
-./venv/bin/pip install -r requirements.txt
+./venv/bin/pip install --quiet -r requirements.txt
 
 # install gunicorn
 ./venv/bin/pip install gunicorn
 
 # Copy systemd files
+echo "Configuring systemd services..."
 sudo cp ./example-confs/oee_celery_beat.service /etc/systemd/system
 sudo cp ./example-confs/oee_server.service /etc/systemd/system
 sudo cp ./example-confs/oee_discovery.service /etc/systemd/system
@@ -31,9 +35,13 @@ sudo systemctl enable oee_server oee_discovery oee_celery oee_celery_beat
 sudo systemctl start oee_server oee_discovery oee_celery oee_celery_beat
 
 # Set up Nginx
+echo "Setting up Nginx..."
 sudo unlink /etc/nginx/sites-enabled/default
 sudo cp ./example-confs/nginx-conf-example /etc/nginx/sites-available
 sudo ln -s /etc/nginx/sites-available/nginx-conf-example /etc/nginx/sites-enabled
+sudo nginx -t
 sudo nginx -s reload
 sudo ufw allow 'Nginx Full'
+
+read -p "Complete. Press any key to continue"
 
