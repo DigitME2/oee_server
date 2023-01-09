24bf053e63f9b0f7eac4c9b25aee45e722acd61d
Sam Bannister <sambannister@gmail.com>
2023-01-09 17:04:50 +0000

    Started adding machine report route

--- app/default/forms.py
+++ app/default/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, FloatField, HiddenField
+from wtforms import StringField, SubmitField, IntegerField, FloatField, HiddenField, TimeField, DateField, SelectField
 from wtforms.validators import DataRequired
+
+from app.visualisation.forms import TIME_FORMAT
 
 
 class StartJobForm(FlaskForm):
@@ -14,3 +16,11 @@ class EndJobForm(FlaskForm):
     quantity_good = IntegerField("Good Quantity")
     rejects = IntegerField("Rejects")
     submit = SubmitField("End")
+
+
+class EditActivityForm(FlaskForm):
+    start_time = TimeField(validators=[DataRequired()], format=TIME_FORMAT)
+    end_time = TimeField(validators=[DataRequired()], format=TIME_FORMAT)
+    submit = SubmitField('Submit')
+    activity_code = SelectField()
+    notes = StringField()
--- app/default/routes.py
+++ app/default/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta, time
+
 from flask import render_template, redirect, url_for, request
 from flask_login import current_user, login_required
 from sqlalchemy import desc
@@ -9,7 +11,8 @@ from app.data_analysis.oee.performance import get_daily_performance_dict, get_da
     get_daily_production_dict
 from app.data_analysis.oee.quality import get_daily_quality_dict
 from app.default import bp
-from app.default.forms import StartJobForm, EndJobForm
+from app.default.forms import StartJobForm, EndJobForm, EditActivityForm
+from app.default.helpers import get_machine_activities, get_jobs
 from app.default.models import ActivityCode, Activity, Machine
 from app.login.models import User
 from config import Config
@@ -29,9 +32,14 @@ def status_page():
     end_job_form = EndJobForm()
     # All activity codes
     activity_codes = ActivityCode.query.all()
-    # jobs_dict =
+    colours_dict = {}
+    for ac in activity_codes:
+        colours_dict[ac.id] = ac.graph_colour
     # Production amounts and reject amounts
-    production_dict, rejects_dict = get_daily_production_dict(human_readable=True)
+    good_dict, rejects_dict = get_daily_production_dict(human_readable=True)
+    production_dict = {}
+    for k, v in good_dict.items():
+        production_dict[k] = good_dict[k] + rejects_dict[k]
     # Availability figure for each machine
     availability_dict = get_daily_machine_availability_dict(human_readable=True)
     # Scheduled uptime for each machine
@@ -46,6 +54,7 @@ def status_page():
     activity_durations_dict = get_daily_activity_duration_dict(human_readable=True)
     return render_template("default/status.html",
                            activity_codes=activity_codes,
+                           colours_dict=colours_dict,
                            machines=machines,
                            start_job_form=start_job_form,
                            end_job_form=end_job_form,
@@ -63,9 +72,32 @@ def status_page():
 
 @bp.route('/machine')
 @login_required
-def machine():
-    # TODO
-    return render_template('default/machine.html')
+def machine_report():
+    """ Show all a user's activities and allow them to be edited """
+    machine_id = request.args.get("machine_id")
+    machine = Machine.query.get_or_404(machine_id)
+    activity_codes = ActivityCode.query.all()
+    requested_date = request.args.get("date", default=datetime.now().date())
+    period_start = datetime.combine(date=requested_date, time=time(hour=0, minute=0, second=0, microsecond=0))
+    period_end = period_start + timedelta(days=1)
+
+    activities = get_machine_activities(machine=machine, time_start=period_start, time_end=period_end)
+    activities.sort(key=lambda a: a.start_time, reverse=True)
+    jobs = get_jobs(time_start=period_start, time_end=period_end, machine=machine)
+    activity_form = EditActivityForm()
+    activity_code_choices = []
+    for ac in ActivityCode.query.all():
+        activity_code_choices.append((str(ac.id), str(ac.short_description)))
+    activity_form.activity_code.choices = activity_code_choices
+
+    return render_template('default/machine.html',
+                           title='Machine Report',
+                           date=requested_date,
+                           machine=machine,
+                           jobs=jobs,
+                           activities=activities,
+                           activity_form=activity_form,
+                           activity_codes=activity_codes)
 
 
 @bp.route('/view_activities')
