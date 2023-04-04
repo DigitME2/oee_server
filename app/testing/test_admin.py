bfb1710835f23c668027b52f1e8cc0f54c6576c8
Sam Bannister <sambannister@gmail.com>
2023-04-04 14:22:36 +0100

    Update tests

--- app/testing/test_admin.py
+++ app/testing/test_admin.py
@@ -8,14 +8,13 @@ class AdminTest(BaseTest):
     def test_routes(self):
         with self.test_client:
             self.app.config['LOGIN_DISABLED'] = True
-            for route in ["/adminhome",
-                          "/settings",
-                          "/newuser",
-                          "/changepassword?user_id=1",
+            for route in ["/admin-home",
+                          "/new-user",
+                          "/change-password?user_id=1&action=reset_password",
                           "/edit-shift",
-                          "/editmachine?machine_id=1",
-                          "/editmachinegroup?machine_group_id=1",
-                          "/editactivitycode?ac_id=1"]:
+                          "/edit-machine?new=True",
+                          "/edit-machine-group?machine_group_id=1",
+                          "/edit-activity-code?ac_id=1"]:
                 response = self.test_client.get(route)
                 self.assertEqual(response.status_code, 200)
 
